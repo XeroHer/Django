ce4ee181802fdd0d34f7e563cda60484265354d5
XeroHer <bksraut27@gmail.com>
2025-06-02 23:38:30 +0100

    todayLearn

--- members/urls.py
+++ members/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('home/', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('skills/', views.skills, name='skills'),
     path('project/', views.project, name='project'),
     path('contact/', views.contact, name='contact'),
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
+    path('testing/', views.testing, name='testing')
 
 ]
--- members/views.py
+++ members/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def home(request):
   context={'name':'Bikesh', 'course':'IT'}
@@ -19,5 +20,38 @@ def project(request):
   return HttpResponse(template.render())
 
 def contact(request):
+  if request.method=="POST":
+    print("This is post")
   template = loader.get_template('contact.html')
+  return HttpResponse(template.render(),request)
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('detail.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def main(request):
+  template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+def testing(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('template.html')
+  context = {
+     'firstname': 'Linus',
+    'fruits': ['Apple', 'Banana', 'Cherry'],  
+    'mymembers': mymembers, 
+  }
+  return HttpResponse(template.render(context, request))
+    
